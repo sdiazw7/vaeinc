92fb0b2c86d6e7cbe232d453099b12d1fdc2324e
Silvia <sdiazw7@gmail.com>
2018-07-18 23:32:12 -0400

    Clean up

--- BigramParser/Models/WordPair.cs
+++ BigramParser/Models/WordPair.cs
@@ -2,12 +2,6 @@
 {
     public class WordPair
     {
-        //public WordPair(string text, int count)
-        //{
-        //    Text = text;
-        //    Count = count;
-        //}
-
         public string Text { get; set; }
         public int Count { get; set; }
     }
